9fa6be1b2054824fcac54d3b1aafb767a892adb3
Krishna Prasad M <krishnaprasad1991@gmail.com>
2023-08-01 16:07:45 +0530

    remove blue border

--- components/feature/sections/Hero.tsx
+++ components/feature/sections/Hero.tsx
@@ -5,7 +5,7 @@ import Button from "../../common/button";
 const HeroSection = () => {
   return (
     <Section id="hero" className="bg-[#f3f4f6] dark:bg-[#181818]">
-      <div style={{ border: "1px solid blue" }}>HeroSection</div>
+      <div>HeroSection</div>
     </Section>
   );
 };
