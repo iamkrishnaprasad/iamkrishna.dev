2d24b651ce9233e1074f53bee97c305339b49a4b
Krishna Prasad M <krishnaprasad1991@gmail.com>
2023-08-04 23:28:05 +0530

    added vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Sections from "@/components/feature/sections";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <>
       <Sections />
+      <Analytics />
     </>
   );
 }
