61299f55d1cc1e6d4495e3e0506493d2523d0085
Krishna Prasad M <krishnaprasad1991@gmail.com>
2023-09-19 23:58:42 +0530

    AboutMe section

--- src/components/feature/sections/AboutMe.tsx
+++ src/components/feature/sections/AboutMe.tsx
@@ -1,9 +1,43 @@
+import Typography from "@/components/common/typography";
 import SectionHeading from "../sectionHeading";
+import moment from "moment";
+import ImageWrapper from "@/components/common/imageWrapper";
+
+const calculateYearOfExperiences = () => {
+  const joiningDate = moment("2019-04-01");
+  const presentDate = moment(new Date());
+
+  const years = presentDate.diff(joiningDate, "years");
+  const months = presentDate.diff(joiningDate, "months") % 12;
+
+  return `${years && `${years}${months ? "+" : ""} years`}`;
+};
 
 const AboutMeContent = () => {
   return (
     <>
-      <SectionHeading title="About Me" subtitle="My introduction" />
+      <SectionHeading title="About Me" />
+      <div className="flex h-full flex-col md:flex-row">
+        <div className="flex h-3/5 w-full items-center justify-center p-4 md:h-full md:w-1/3 md:p-16">
+          <div className="flex w-3/5 md:w-[85%]">
+            <ImageWrapper
+              src={"/assets/images/profile-01.jpg"}
+              alt="Profile Image"
+              width={128}
+              height={128}
+              className="max-h-full max-w-full"
+            />
+          </div>
+        </div>
+        <div className="flex h-2/5 w-full items-start justify-center p-4 md:h-full md:w-2/3 md:items-center md:p-16">
+          <Typography variant="body1">
+            I am a full-stack developer offering {calculateYearOfExperiences()} of experience
+            providing high-impact web solutions for diverse industry organizations. Skilled in
+            designing, developing and testing multiple web-based applications incorporating a range
+            of technologies.
+          </Typography>
+        </div>
+      </div>
     </>
   );
 };
